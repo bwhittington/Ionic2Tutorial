071a42a9e2f923edac2813762b101874ac215c5e
Brett Whittington <BWhittington@hmbnet.com>
2017-03-13 16:28:27 -0400

    Commit

---
+++ IonicTwoTutorial/src/app/courses/courses.spec.ts
@@ -0,0 +1,19 @@
+﻿import { ComponentFixture, async } from '@angular/core/testing';
+
+import { TestUtils } from '../../test';
+import { Courses } from './courses.module';
+
+let fixture: ComponentFixture<Courses> = null;
+let instance: any = null;
+
+describe('Component: Courses', () => {
+
+    beforeEach(async(() => TestUtils.beforeEachCompiler([Courses]).then(compiled => {
+        fixture = compiled.fixture;
+        instance = compiled.instance;
+    })));
+
+    it('should create the courses page', async(() => {
+        expect(instance).toBeTruthy();
+    }));
+});
