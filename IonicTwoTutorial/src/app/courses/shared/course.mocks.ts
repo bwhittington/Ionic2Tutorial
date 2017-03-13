cc91412c6630f88aa9d77e35a0653c8a320a8961
Brett Whittington <brett.whittington@gmail.com>
2017-03-13 16:35:31 -0400

    Commit new changes

--- IonicTwoTutorial/src/app/courses/shared/course.mocks.ts
+++ IonicTwoTutorial/src/app/courses/shared/course.mocks.ts
@@ -1,12 +1,12 @@
 ﻿import { Course } from './course.model';
 
 export const courses: Course[] = [
-    { id: 1, name: 'C#/.NET', syllabus:'' },
+    { id: 1, name: 'C#/.NET', syllabus: '' },
     { id: 2, name: 'HTML/CSS/Responsive', syllabus: '' },
     { id: 3, name: 'MVC/Web API', syllabus: '' },
     { id: 4, name: 'Java', syllabus: '' },
     { id: 5, name: 'JavaScript', syllabus: '' },
     { id: 6, name: 'SQL', syllabus: '' },
     { id: 7, name: 'Cordova', syllabus: '' },
-    { id: 8, name: 'Consulting Book', syllabus: '' },
-]
+    { id: 8, name: 'Consulting Book', syllabus: '' }
+];
---
+++ IonicTwoTutorial/src/app/courses/tests/course-list.spec.ts
@@ -0,0 +1,20 @@
+﻿import { ComponentFixture, async } from '@angular/core/testing';
+import { } from 'jasmine';
+
+import { TestUtils } from './test';
+import { CourseList } from '../course-list/course-list.component';
+
+let fixture: ComponentFixture<CourseList> = null;
+let instance: any = null;
+
+describe('Component: CourseList', () => {
+
+    beforeEach(async(() => TestUtils.beforeEachCompiler([CourseList]).then(compiled => {
+        fixture = compiled.fixture;
+        instance = compiled.instance;
+    })));
+
+    it('should create the hello ionic page', async(() => {
+        expect(instance).toBeTruthy();
+    }));
+});
